ae0f4c1ad2d996f84c8a5b04f2111153586524d5
BCS <christiansuinyuy@gmail.com>
2025-10-02 00:16:56 +0100

    added facebook link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ function Footer(){
     return <footer className="shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1)] relative
      bottom-2 items-center h-20 mt-10 justify-around w-full flex flex-col">
         <div className="socials gap-3 flex h-8">
-            <a href="#" className="w-10">
+            <a href="https://www.facebook.com/christian.bcs.521749/" className="w-10">
                 <img src="/public/images/Facebook png logo transparent png logo.jpg" alt="Facebook" />
             </a>
             <a href="https://www.linkedin.com/in/christian-suinyuy-6679b2327" target="blank" className="w-10">
