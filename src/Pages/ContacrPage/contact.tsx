5b35cc1af9d9d662b306ebf1e70aad6e07a8f8f5
BCS <christiansuinyuy@gmail.com>
2025-10-02 00:15:50 +0100

    fixed the contact form

--- src/Pages/ContacrPage/contact.tsx
+++ src/Pages/ContacrPage/contact.tsx
@@ -1,32 +1,43 @@
-function Contact(){
-    return (
-        <section className="flex justify-center flex-col items-center gap-9 min-h-135">
-            <div id="hero" className="text-center flex flex-col gap-2">
-                <h1>Get in Touch</h1>
-                <p className="md:w-160">I`m always open to discaussing new projects, creative ideas, or opurtuinities, fell
-                    free to reach out, and i`ll get back to you as soon an possible.    </p>
-            </div>
+// ContactForm.jsx
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-            <form action="#" className="w-3/4 sm:w-100 flex flex-col gap-3 ">
-                <label htmlFor="name">
-                    <p className="font-semibold">Your name</p>
-                    <input type="text" placeholder="Enter your name" className="p-1 focus:outline-blue-300 placeholder:to-white border border-gray-400 w-full" />
-                </label>
+const Contact = () => {
+  const form = useRef<HTMLFormElement>(null);
 
-                <label htmlFor="name">
-                    <p className="font-semibold">Your Email</p>
-                    <input type="email" placeholder="Enter your email" className="p-1 focus:outline-blue-300 placeholder:to-white border border-gray-400 w-full" />
-                </label>
-
-                <label htmlFor="message">
-                    <p className="font-semibold">Your Email</p>
-                    <textarea name="message" id="message" placeholder="Message to send" className="border focus:outline-blue-300 p-1 placeholder:to-white border-gray-400 w-full min-h-20"></textarea>
-                </label>
-
-                <button type="submit" className="p-2 rounded bg-blue-600 mx-auto">Send Message</button>
-            </form>
-        </section>
+  const sendEmail = (e:any) => {
+    e.preventDefault();
+    if (form.current)
+    emailjs.sendForm(
+      'service_99afos7',
+      'template_z3lzet8',
+      form.current,
+      'IpbpvYAPgPg1vPr2T'
     )
-}
+    .then(() => {
+      alert('Message sent successfully!');
+    })
+    .catch((error) => {
+      alert('Failed to send message: ' + error.text);
+    });
+  };
 
-export default Contact
+  return (
+    <form ref={form} onSubmit={sendEmail} className='grid mx-4 sm:mx-30 md:mx-50 gap-2'>
+      <label>
+        Name
+        <input type="text" name="from_name" className='border focus:outline-pink-300 p-2 w-full placeholder:to-white' placeholder='Enter your name here' required />
+      </label>
+
+      <label>Email</label>
+      <input type="email" name="from_email" className='border focus:outline-pink-300 p-2 w-full placeholder:to-white' required />
+
+      <label>Message</label>
+      <textarea name="message" required  className='border focus:outline-pink-300 p-2 w-full placeholder:to-white'/>
+
+      <button type="submit" className='bg-blue-300 p-3 rounded'>Send</button>
+    </form>
+  );
+};
+
+export default Contact;
